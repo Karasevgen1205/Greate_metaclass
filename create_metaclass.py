599509a4b736920325354ebd29f508b12c3b5a75
Karasevgen1205 <71278131+Karasevgen1205@users.noreply.github.com>
2021-01-31 22:52:22 +0300

    Update create_metaclass.py

--- create_metaclass.py
+++ create_metaclass.py
@@ -7,7 +7,7 @@ def get_last_name(self):
 def get_middle_name(self):
     return self.middle_name
 
-def __init__(self, first_name, last_name, middle_name):
+def init(self, first_name, last_name, middle_name):
     self.first_name = first_name
     self.last_name = last_name
     self.middle_name = middle_name
@@ -23,5 +23,22 @@ attr = {
     'last_name': '',
     'middle_name': '',
     'get_first_name': get_first_name,
-    
+    'get_last_name': get_last_name,
+    'get_middle_name': get_middle_name,
+    '__init__': init,
 }
+
+bases = (
+    BaseUser,
+)
+
+User = type('User', bases, attr)
+
+user1 = User('John', 'Test', 'Te100vi4')
+
+print(str(user1))
+print(user1.get_first_name())
+print(user1.get_last_name())
+print(user1.get_middle_name())
+
+
